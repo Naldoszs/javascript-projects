05e0b816562e753ad9c4787aacd8c8271f665921
Adeyeye Niyi Festus <adeyeyeniyi2@gmail.com>
2024-12-30 09:38:20 +0100

    semi-finished my modal project

--- modal-project/setup/app.js
+++ modal-project/setup/app.js
@@ -1,4 +1,24 @@
 // select modal-btn,modal-overlay,close-btn
+const modalBtnEl = document.querySelector(".js-modal-btn");
+
+const modalOverlayEl = document.querySelector(".js-modal-overlay");
+
+const modalCloseBtnEl = document.querySelector(".js-close-btn");
+
 // listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+modalBtnEl.addEventListener("click", openModal);
+modalCloseBtnEl.addEventListener("click", closeModal);
+
+// function to open modal
+function openModal() {
+  if (!modalOverlayEl.classList.contains("open-modal")) {
+    modalOverlayEl.classList.add("open-modal");
+  }
+}
+
+// function to remove modal
+function closeModal() {
+  if (modalOverlayEl.classList.contains("open-modal")) {
+    modalOverlayEl.classList.remove("open-modal");
+  }
+}
